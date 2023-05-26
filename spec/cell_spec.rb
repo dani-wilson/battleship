bc494507694d407c58d6471a4c06943c87f3767c
Anna Wiley <awiley33@gmail.com>
2023-05-26 08:18:54 -0500

    test: Establish test suite with before block, add test for exists and has a coordinate

---
+++ spec/cell_spec.rb
@@ -0,0 +1,14 @@
+require './spec/spec_helper'
+
+
+RSpec.describe Cell do
+
+  before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell = Cell.new("B4")
+  end
+
+  it 'exists and has a coordinate' do
+    expect(cell.coordinate).to eq(B4)
+  end
+end

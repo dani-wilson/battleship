9ddff092591d1aaedac9a01eed063213b2017fc8
Anna Wiley <awiley33@gmail.com>
2023-05-26 08:31:54 -0500

    Add ship instance var and empty? method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,18 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate,
+              :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
   end
-  
+
+  def empty?
+    if !@ship
+      true
+    else
+      false
+    end
+  end
+
 end

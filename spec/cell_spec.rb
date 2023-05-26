cab27179af279c31429d64aae53c979aafc3e568
Anna Wiley <awiley33@gmail.com>
2023-05-26 08:31:28 -0500

    test: Add test for ship and empty? methods

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -11,4 +11,9 @@ RSpec.describe Cell do
   it 'exists and has a coordinate' do
     expect(@cell.coordinate).to eq("B4")
   end
+
+  it 'has no ship by default, so is an empty cell' do
+    expect(@cell.ship).to eq(nil)
+    expect(@cell.empty?).to eq(true)
+  end
 end

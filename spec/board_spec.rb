85f71326313c7a5d944db09205c1f51f3b37bfb8
Anna Wiley <awiley33@gmail.com>
2023-05-27 11:33:09 -0500

    test: Add test for cells initialized

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe do
   it 'exists' do
     expect(@board).to be_a(Board)
   end
+
+  it 'has 16 cells' do
+    expect(@board.cells.count).to eq(16)
+    expect(@board.cells).to be_a(Hash)
+  end
+
+
 end

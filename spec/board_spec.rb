cfc156f06c4342416f807f0161a8a47b68e02859
Dani Wilson <danirae.powell@gmail.com>
2023-05-28 15:27:03 -0600

    test: Remove changes to valid placement tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -32,11 +32,11 @@ RSpec.describe do
   end
 
   it 'will validate that coordinates are consecutive' do
-    expect(@board.consecutive_check?(@cruiser, ["A1", "A2", "A3"])).to be(false)
-    expect(@board.consecutive_check?(@submarine, ["A1", "C1"])).to be(false)
-    expect(@board.consecutive_check?(@cruiser, ["A3", "A2", "A1"])).to be(false)
-    expect(@board.consecutive_check?(@submarine, ["C1", "B1"])).to be(false)
-    expect(@board.consecutive_check?(@submarine, ["A1", "A2"])).to be(true)
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be(false)
+    expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be(false)
+    expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be(false)
+    expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be(false)
+    expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be(true)
   end
 
 

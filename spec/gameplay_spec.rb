0bae46651199b4a35cf6e95420197cb3ef37a042
Anna Wiley <awiley33@gmail.com>
2023-05-30 16:13:56 -0500

    test: Add tests for beginning game

--- spec/gameplay_spec.rb
+++ spec/gameplay_spec.rb
@@ -6,8 +6,21 @@ RSpec.describe Gameplay do
   before(:each) do
     @game = Gameplay.new
   end
-  
+
   it 'exists' do
     expect(@game).to be_a(Gameplay)
+  end
+
+  it 'has a welcome message' do
+    expect(@game.message).to eq("Welcome to BATTLESHIP
+    Enter p to play. Enter q to quit.")
+  end
+
+  it 'offers option to start or quit game' do
+    expect(@game.start_or_quit).to eq(@game.message)
+  end
+
+  it 'allows user to play' do
+    expect(@game.begin_game).to eq("p")
   end
 end

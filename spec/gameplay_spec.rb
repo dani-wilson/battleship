b49614868c51d0da452f34e741d9fb7371148d68
Dani Wilson <danirae.powell@gmail.com>
2023-05-30 20:13:57 -0600

    test: Add fake tests for pry

--- spec/gameplay_spec.rb
+++ spec/gameplay_spec.rb
@@ -11,16 +11,24 @@ RSpec.describe Gameplay do
     expect(@game).to be_a(Gameplay)
   end
 
-  it 'has a welcome message' do
-    expect(@game.message).to eq("Welcome to BATTLESHIP
-    Enter p to play. Enter q to quit.")
+  # it 'has a welcome message' do
+  #   expect(@game.message).to eq("Welcome to BATTLESHIP
+  #   Enter p to play. Enter q to quit.")
+  # end
+
+  # it 'offers option to start or quit game' do
+  #   expect(@game.start_or_quit).to eq(@game.message)
+  # end
+
+  # it 'allows user to play' do
+  #   expect(@game.begin_game).to eq("p")
+  # end
+
+  it 'test' do
+    @game.user_turn
   end
 
-  it 'offers option to start or quit game' do
-    expect(@game.start_or_quit).to eq(@game.message)
-  end
-
-  it 'allows user to play' do
-    expect(@game.begin_game).to eq("p")
+  it 'test' do
+    @game.computer_turn
   end
 end

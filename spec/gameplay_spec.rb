e1921424c6384d62f518a319ccde00ba17927075
Dani Wilson <127890878+dani-wilson@users.noreply.github.com>
2023-06-01 13:02:55 -0600

    Update gameplay_spec.rb
    fix: Remove unnecessary tests

--- spec/gameplay_spec.rb
+++ spec/gameplay_spec.rb
@@ -19,31 +19,4 @@ RSpec.describe Gameplay do
     expect(@game.computer_cruiser).to be_a(Ship)
     expect(@game.computer_sub).to be_a(Ship)
   end
-
-  # it 'has a welcome message' do
-  #   expect(@game.message).to eq("Welcome to BATTLESHIP
-  #   Enter p to play. Enter q to quit.")
-  # end
-
-  # it 'offers option to start or quit game' do
-  #   expect(@game.start_or_quit).to eq(@game.message)
-  # end
-
-  # it 'allows user to play' do
-  #   expect(@game.begin_game).to eq("p")
-  # end
-
-
-  # it 'test' do
-  #   @game.computer_get_coordinates
-  #   expect(@game.computer_get_coordinates).to be_an(Array)
-  # end
-
-  it 'can place ships on the computer' do
-    @game.computer_place_ship(@game.computer_cruiser)
-    @game.computer_place_ship(@game.computer_sub)
-
-    expect(@game.computer_board.render_board(true))
-    # expect rendering to visually print 2 ships in valid coordinates
-  end
 end
